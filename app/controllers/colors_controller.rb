fd3cf12c81bd97aff32ef48104d6bec062ab45e2
James Lieu <j.lieu888@gmail.com>
2013-10-01 18:07:43 +0100

    Added buttons to the view

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -14,7 +14,24 @@ class ColorsController < UIViewController
       UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin
     self.view.addSubview(@label)
     self.title = "Colors"
+
+    ["red", "green", "blue"].each_with_index do |color_text, index| color = UIColor.send("#{color_text}Color")
+      button_width = 80
+      button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+      button.setTitle(color_text, forState:UIControlStateNormal)
+      button.setTitleColor(color, forState:UIControlStateNormal)
+      button.sizeToFit
+      button.frame = [
+      [30 + index*(button_width + 10),
+       @label.frame.origin.y + button.frame.size.height + 30],
+      [80, button.frame.size.height]
+    ]
+    button.autoresizingMask =
+      UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin
+    button.addTarget(self,
+      action:"tap_#{color_text}",
+      forControlEvents:UIControlEventTouchUpInside)
+    self.view.addSubview(button)
+    end
   end
-
-
 end
