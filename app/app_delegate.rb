a844c455986aec5c8e5d1ebe9920c5bd17acba77
Michihito Shigemura <i.am.shige@gmail.com>
2013-10-02 13:01:40 +0900

    Delete trailing whitespace

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,11 +3,11 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
 
-    # @window.rootViewController = 
+    # @window.rootViewController =
     # ColorsController.alloc.initWithNibName(nil, bundle: nil)
     controller = ColorsController.alloc.initWithNibName(nil, bundle: nil)
     nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
-    
+
     @window.rootViewController = nav_controller
 
     true
--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -7,10 +7,10 @@ class ColorsController < UIViewController
     @label = UILabel.alloc.initWithFrame(CGRectZero)
     @label.text = "Colors"
     @label.sizeToFit
-    @label.center = 
-    [self.view.frame.size.width / 2,
-    self.view.frame.size.height / 2]
-    @label.autoresizingMask = 
+    @label.center =
+      [self.view.frame.size.width / 2,
+       self.view.frame.size.height / 2]
+    @label.autoresizingMask =
       UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin
     self.view.addSubview(@label)
     self.title = "Colors"
@@ -22,21 +22,21 @@ class ColorsController < UIViewController
       button.setTitleColor(color, forState:UIControlStateNormal)
       button.sizeToFit
       button.frame = [
-      [30 + index*(button_width + 10),
-       @label.frame.origin.y + button.frame.size.height + 30],
-      [80, button.frame.size.height]
-    ]
-    button.autoresizingMask =
-      UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin
-    button.addTarget(self,
-      action:"tap_#{color_text}",
+                      [30 + index*(button_width + 10),
+                       @label.frame.origin.y + button.frame.size.height + 30],
+                      [80, button.frame.size.height]
+                     ]
+      button.autoresizingMask =
+        UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin
+      button.addTarget(self,
+                       action:"tap_#{color_text}",
       forControlEvents:UIControlEventTouchUpInside)
     self.view.addSubview(button)
     end
   end
 
   def tap_red
-    controller = ColorDetailController.alloc.initWithColor(UIColor.redColor) 
+    controller = ColorDetailController.alloc.initWithColor(UIColor.redColor)
     self.navigationController.pushViewController(controller, animated: true)
   end
 
@@ -44,7 +44,7 @@ class ColorsController < UIViewController
     controller = ColorDetailController.alloc.initWithColor(UIColor.greenColor)
     self.navigationController.pushViewController(controller, animated: true)
   end
-  
+
   def tap_blue
     controller = ColorDetailController.alloc.initWithColor(UIColor.blueColor)
     self.navigationController.pushViewController(controller, animated: true)
